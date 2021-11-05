4c59ceed87fe75117dbfe2687fd50a51baae114c
Korlan Omarova <omar_korlan@mail.ru>
2021-11-05 22:19:44 +0600

    feature: completing 8 lab

--- sales/src/main/java/kz/iitu/sales/SalesApplication.java
+++ sales/src/main/java/kz/iitu/sales/SalesApplication.java
@@ -3,11 +3,13 @@ package kz.iitu.sales;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
 public class SalesApplication {
 
     public static void main(String[] args) {
