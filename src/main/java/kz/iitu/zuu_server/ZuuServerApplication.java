7355b8449c0a33e9404764065604e84cbed910e0
aitmoldanov <60466089+aitmoldanov@users.noreply.github.com>
2021-11-14 23:43:13 +0600

    token generator added

--- src/main/java/kz/iitu/zuu_server/ZuuServerApplication.java
+++ src/main/java/kz/iitu/zuu_server/ZuuServerApplication.java
@@ -2,8 +2,12 @@ package kz.iitu.zuu_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient    // It acts as a eureka client
+@EnableZuulProxy
 public class ZuuServerApplication {
 
     public static void main(String[] args) {
