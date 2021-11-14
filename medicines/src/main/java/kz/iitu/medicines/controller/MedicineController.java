be31bd7575302857369b7c59a31f3355e8aea84a
assem22 <60426080+assem22@users.noreply.github.com>
2021-11-14 23:34:52 +0600

    changes

--- medicines/src/main/java/kz/iitu/medicines/controller/MedicineController.java
+++ medicines/src/main/java/kz/iitu/medicines/controller/MedicineController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/medicines")
+@RequestMapping("/")
 @Api(value = "Medicine Controller class", description = "The class allows to interact with Medicine base.")
 public class MedicineController {
     @Autowired
