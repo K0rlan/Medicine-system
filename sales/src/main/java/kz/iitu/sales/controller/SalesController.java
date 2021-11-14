104be02d4b0f34929727f9309ddb42d3af7650e1
assem22 <60426080+assem22@users.noreply.github.com>
2021-11-14 23:35:39 +0600

    changes

--- sales/src/main/java/kz/iitu/sales/controller/SalesController.java
+++ sales/src/main/java/kz/iitu/sales/controller/SalesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/medicines/discount")
+@RequestMapping("/")
 @Api(value = "Sales Controller class", description = "The class allows to interact with medicine discounts.")
 public class SalesController {
     @Autowired
