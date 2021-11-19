f5297f3567c0b42d0b820c190216d2e87bd2c82f
Korlan Omarova <omar_korlan@mail.ru>
2021-11-19 22:48:49 +0600

    feature: Secure Service-to-Service Requests

--- medicines/src/main/java/kz/iitu/medicines/services/impl/SalesServiceImpl.java
+++ medicines/src/main/java/kz/iitu/medicines/services/impl/SalesServiceImpl.java
@@ -7,7 +7,10 @@ import kz.iitu.medicines.services.SalesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.http.HttpEntity;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 @Service
 public class SalesServiceImpl implements SalesService {
 
@@ -23,7 +26,16 @@ public class SalesServiceImpl implements SalesService {
                     @HystrixProperty(name = "maxQueueSize", value = "50"),
             })
     public Double getDiscountByMedicineCategory(Category category) {
-        return restTemplate.getForObject("http://discount-service/medicines/discount/category/" + category, Double.class);
+//        return restTemplate.getForObject("http://discount-service/medicines/discount/category/" + category, Double.class);
+        String apiCredentials = "sales-client:sales";
+        String base64Credentials = new String(Base64.encodeBase64(apiCredentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Credentials);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange("http://discount-service/medicines/discount/category/" + category,
+                HttpMethod.GET, entity, Double.class).getBody();
     }
 
     public Double getDiscountByMedicineCategoryFallback(Category category) {
