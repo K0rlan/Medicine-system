cfccf6fb96b61f3e5b1d57c708948436f5429f3c
assem22 <60426080+assem22@users.noreply.github.com>
2021-11-29 12:06:01 +0600

    db

--- microservices/order/src/main/java/kz/iitu/order/model/Medicine.java
+++ microservices/order/src/main/java/kz/iitu/order/model/Medicine.java
@@ -3,11 +3,15 @@ package kz.iitu.order.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "medicine")
 public class Medicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String dosage;
--- microservices/order/src/main/java/kz/iitu/order/model/Order.java
+++ microservices/order/src/main/java/kz/iitu/order/model/Order.java
@@ -4,15 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Double totalCost;
+
     private List<Medicine> medicines;
     private Customer customer;
-    private Double totalCost;
 }
